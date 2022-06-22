fb171ad01c3af21f5c69eec581e097a91674f62a
Łukasz Woźniak <grafzero73@gmail.com>
2022-06-22 23:18:25 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './styles/style.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>Virtual Art Exhibition</h1>
+    )
+  }
+}
